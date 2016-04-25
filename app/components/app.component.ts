b8b4d5f0c109cc118aa16447b2d05cb68f3d0009
Catherine Mackenzie <cate.c.mackenzie@gmail.com>
2016-04-24 17:33:24 -0700

    Adds login-form.component skeleton

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,7 +1,12 @@
 import {Component} from 'angular2/core';
+import {LoginFormComponent} from './login-form.component'
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  template: `
+<h1>My First Angular 2 App</h1>
+<login-form></login-form>
+`,
+  directives: [LoginFormComponent]
 })
 export class AppComponent { }
---
+++ app/components/login-form.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core'
+import {NgForm} from 'angular2/common'
+
+@Component({
+  selector: 'login-form',
+  templateUrl: 'app/templates/login-form.component.html'
+})
+export class LoginFormComponent {
+  
+}
